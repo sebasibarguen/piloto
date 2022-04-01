5dc7fbcedda9551b1bd7fa8c2c56a5b335f56f82
Sebastian Ibarguen <sebas@vana.gt>
2022-04-01 17:12:08 -0600

    Add basic backend.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "piloto": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "hosting": {
+        "S3AndCloudFront": {
+            "Region": "string",
+            "HostingBucketName": "string",
+            "WebsiteURL": "string",
+            "S3BucketSecureURL": "string"
+        }
+    },
+    "auth": {
+        "piloto01c352f6": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
